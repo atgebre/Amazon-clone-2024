d4bc9455a8eb5010283ec97ca663b0625a272c1d
atgebre <gebreatklit@gmail.com>
2024-10-31 15:50:59 -0400

    ADD BaseUrl

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const axiosInstance = axios.create({
   
   // baseURL: "http://127.0.0.1:5001/clone-a862d/us-central1/api",
-   baseURL:"http://localhost:5000",
+  //  baseURL:"http://localhost:5000",
+   baseURL:"https://amazon-clone-2024-827l.onrender.com",
 
 });
 
